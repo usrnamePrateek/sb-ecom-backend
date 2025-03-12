e4670c92a223b05cd26416f0d12dc7e7c8c10db9
usrnamePrateek <usernameprateek@gmail.com>
2025-03-13 00:08:19 +0530

    cors changes

--- src/main/java/com/ecommerce/ecom/security/WebConfig.java
+++ src/main/java/com/ecommerce/ecom/security/WebConfig.java
@@ -9,15 +9,15 @@ import java.util.List;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-//    @Override
-//    public void addCorsMappings(CorsRegistry registry) {
-//       registry.addMapping("/**")
-//               .allowedOrigins("http://localhost:5173/")
-//               .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-//               .allowedHeaders("*")
-//               .allowCredentials(true);
-//
-//    }
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+       registry.addMapping("/**")
+               .allowedOrigins("https://sb-ecom-frontend.vercel.app/")
+               .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+               .allowedHeaders("*")
+               .allowCredentials(true);
+
+    }
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
